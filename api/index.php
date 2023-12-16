ace76729b852c229b0df65cafc2f20995c2e077b
John Julius Pattikaihatu <john.julius@ti.ukdw.ac.id>
2023-12-16 19:01:30 +0700

    aa

--- api/index.php
+++ api/index.php
@@ -1,15 +1,4 @@
-import { VercelRequest, VercelResponse } from '@vercel/node';
-import { sql } from '@vercel/postgres';
- 
-export default async function handler(
-  request: VercelRequest,
-  response: VercelResponse,
-) {
-  try {
-    const result =
-      await sql`CREATE TABLE Pets ( Name varchar(255), Owner varchar(255) );`;
-    return response.status(200).json({ result });
-  } catch (error) {
-    return response.status(500).json({ error });
-  }
-}
+<?php
+
+// Forward Vercel requests to normal index.php
+require __DIR__ . '/../public/index.php';
